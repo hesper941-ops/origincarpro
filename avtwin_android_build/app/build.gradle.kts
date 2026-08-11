dda0daeef1b5bace77db2b1ab6248d9bd3c8f4b6
WR1code <3394395308@qq.com>
2026-08-11 23:23:14 +0800

    Bump AV-Twin responder to v0.2

--- avtwin_android_build/app/build.gradle.kts
+++ avtwin_android_build/app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -13,18 +11,16 @@ android {
         applicationId = "com.example.avtwinresponder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
 
-kotlin {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
