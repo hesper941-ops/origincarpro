88b499e8484906d1b6b7b3ba4e217d5f10b57494
WR1code <3394395308@qq.com>
2026-08-12 15:23:02 +0800

    Allow writing probe/debug WAVs to SAF streams

--- avtwin_android_build/app/src/main/java/com/example/avtwinresponder/WavWriter.kt
+++ avtwin_android_build/app/src/main/java/com/example/avtwinresponder/WavWriter.kt
@@ -2,33 +2,37 @@ package com.example.avtwinresponder
 
 import java.io.File
 import java.io.FileOutputStream
+import java.io.OutputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 object WavWriter {
     fun writeMonoPcm16(file: File, samples: ShortArray, sampleRate: Int) {
         file.parentFile?.mkdirs()
-        val dataBytes = samples.size * 2
-        FileOutputStream(file).use { out ->
-            val header = ByteBuffer.allocate(44).order(ByteOrder.LITTLE_ENDIAN)
-            header.put("RIFF".toByteArray(Charsets.US_ASCII))
-            header.putInt(36 + dataBytes)
-            header.put("WAVE".toByteArray(Charsets.US_ASCII))
-            header.put("fmt ".toByteArray(Charsets.US_ASCII))
-            header.putInt(16)
-            header.putShort(1)
-            header.putShort(1)
-            header.putInt(sampleRate)
-            header.putInt(sampleRate * 2)
-            header.putShort(2)
-            header.putShort(16)
-            header.put("data".toByteArray(Charsets.US_ASCII))
-            header.putInt(dataBytes)
-            out.write(header.array())
+        FileOutputStream(file).use { out -> writeMonoPcm16(out, samples, sampleRate) }
+    }
 
-            val buf = ByteBuffer.allocate(samples.size * 2).order(ByteOrder.LITTLE_ENDIAN)
-            for (s in samples) buf.putShort(s)
-            out.write(buf.array())
-        }
+    fun writeMonoPcm16(out: OutputStream, samples: ShortArray, sampleRate: Int) {
+        val dataBytes = samples.size * 2
+        val header = ByteBuffer.allocate(44).order(ByteOrder.LITTLE_ENDIAN)
+        header.put("RIFF".toByteArray(Charsets.US_ASCII))
+        header.putInt(36 + dataBytes)
+        header.put("WAVE".toByteArray(Charsets.US_ASCII))
+        header.put("fmt ".toByteArray(Charsets.US_ASCII))
+        header.putInt(16)
+        header.putShort(1)
+        header.putShort(1)
+        header.putInt(sampleRate)
+        header.putInt(sampleRate * 2)
+        header.putShort(2)
+        header.putShort(16)
+        header.put("data".toByteArray(Charsets.US_ASCII))
+        header.putInt(dataBytes)
+        out.write(header.array())
+
+        val buf = ByteBuffer.allocate(samples.size * 2).order(ByteOrder.LITTLE_ENDIAN)
+        for (s in samples) buf.putShort(s)
+        out.write(buf.array())
+        out.flush()
     }
 }
