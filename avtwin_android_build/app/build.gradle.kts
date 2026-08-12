493b980530a50cee89331c210294628e8447b128
WR1code <3394395308@qq.com>
2026-08-12 18:05:46 +0800

    Bump strict ARM responder to v0.8.2

--- avtwin_android_build/app/build.gradle.kts
+++ avtwin_android_build/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.avtwinresponder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.8.1"
+        versionCode = 10
+        versionName = "0.8.2"
     }
 
     compileOptions {
