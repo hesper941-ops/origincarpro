e028481fdc25121ad3bd1ce6b37aa8201408e573
WR1code <3394395308@qq.com>
2026-08-12 16:27:48 +0800

    Bump responder hotfix version to 0.8.1

--- avtwin_android_build/app/build.gradle.kts
+++ avtwin_android_build/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.avtwinresponder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.8.0"
+        versionCode = 9
+        versionName = "0.8.1"
     }
 
     compileOptions {
